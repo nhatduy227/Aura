dd5e3605eb0aa8f8b2880db5f957fe5c83612772
AriNguyen3 <nn389@drexel.edu>
2021-02-14 07:09:00 -0500

    DONE-describe-image

---
+++ ModeSelection/descibeImage.py
@@ -0,0 +1,84 @@
+import pickle
+import json
+import sys
+
+import numpy as np
+
+from keras.models import load_model
+from keras.applications.inception_v3 import preprocess_input
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.image import img_to_array
+
+from PIL import Image
+import requests
+from io import BytesIO
+
+import traceback
+
+
+def init():
+    global caption_model
+    global tokenizer
+    global encode_model
+    global model_path
+
+    global MAX_LEN
+    global OUTPUT_DIM
+    global WIDTH
+    global HEIGHT
+
+    MAX_LEN = 51
+    OUTPUT_DIM = 2048
+    WIDTH = 299
+    HEIGHT = 299
+
+    caption_model = load_model("../model/caption_model.h5")
+    encode_model = load_model("../model/encode_model.h5")
+
+    with open('../model/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+
+def run(img_url):
+    try:
+        caption = "startseq"
+
+        img = Image.open(img_url)
+        img = img.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
+        img = img_to_array(img)
+
+        # preprocess the image
+
+        img = preprocess_input(img)
+        img = np.expand_dims(img, axis=0)
+
+        x1 = encode_model.predict(img)
+        x1 = x1.reshape((1, OUTPUT_DIM))
+
+        # generate the caption
+
+        for i in range(MAX_LEN):
+            seq = tokenizer.texts_to_sequences([caption])
+            x2 = pad_sequences(seq, maxlen=MAX_LEN)
+
+            y = caption_model.predict([x1, x2], verbose=0)
+            word = tokenizer.index_word[np.argmax(y)]
+
+            if word == "endseq":
+                break
+
+            caption += " " + word
+
+        caption = caption.replace("startseq", "").strip()
+        print(caption)
+        return {"caption": caption}
+
+    except Exception as e:
+        print(traceback.format_exc())
+        return {"error": str(e)}
+
+
+if __name__ == '__main__':
+    init()
+    run("../img/mr-robot1.jpg")
